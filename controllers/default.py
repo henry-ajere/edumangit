83f2c25938063e20711e8fb49f7b3cbbb80e155b
Henry Ajere <henro.ajere@gmail.com>
2015-05-26 16:25:03 +0100

    change result by semester-extend-use json dumps-01

--- controllers/default.py
+++ controllers/default.py
@@ -408,7 +408,8 @@ def getResult():
             totalcu = db(query)(db.registered_course.sessions==session).select(sumcu).first()[sumcu] #still need to filter for semester
             totalwgp = db(query)(db.registered_course.sessions==session).select(sumwgp).first()[sumwgp] #still need to filete for semester
 
-            result.append(db(query)(db.registered_course.sessions==session)(db.registered_course.semester==semester.semester).select())
+            #result.append(db(query)(db.registered_course.sessions==session)(db.registered_course.semester==semester.semester).select())
+            result = db(query)(db.registered_course.sessions==session)(db.registered_course.semester==semester.semester).select()
             resultdumps = json.dumps({'results' : result})
 
             semesterlist.extend(resultdumps)
